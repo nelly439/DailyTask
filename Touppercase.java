88c3817153144a3477b06c98e4a7ac286541f0ab
nelly439 <nebenezer86@gmail.com>
2025-10-23 07:57:17 +0100

    first commit

---
+++ Touppercase.java
@@ -0,0 +1,10 @@
+public class UpperCase{
+   public static void main(String [] args){
+
+   String name = "the lord is good";
+   String uppercase = name.toUpperCase();
+
+   System.out.println("UpperCase :" + uppercase);
+
+}
+}
