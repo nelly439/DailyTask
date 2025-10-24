71627637bd30195c88241a1b368fb37f802a31fa
nelly439 <nebenezer86@gmail.com>
2025-10-24 10:51:18 +0100

    initial commit

---
+++ TouppercaseTest.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from Touppercase import *
+
+class Touppercase(TestCase):
+	def test_that_the_input_is_a_string(self):
+		expected = "Nelson"
+		actual = 1234
+			assert.Equal(expected,actual)
+		
+
+
